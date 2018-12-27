29a18c6bf03be37d8bff6728765f248efc247968
lukashevaev <46032891+lukashevaev@users.noreply.github.com>
2018-12-27 17:19:33 +0300

    Update and rename Menu.cpp to Menu.h

---
+++ Menu.h
@@ -0,0 +1,63 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include <time.h>
+#include "windows.h"
+#include <sstream>
+#include "Record.h"
+
+class Menu
+{
+public:;
+	   int menu() {
+		   RenderWindow window(VideoMode(500, 750), "Menu");
+
+		   Texture t;
+		   t.loadFromFile("images/tetris.jpg");
+
+		   Sprite s(t);
+
+		   Game play;
+
+		   Record record;
+
+		   int maxscore = 0;
+		   int scores = 0;
+		   while (window.isOpen())
+		   {
+			   maxscore = record.read();
+			   Event e;
+			   while (window.pollEvent(e))
+			   {
+				   if (e.type == Event::Closed)
+					   window.close();
+				   if (e.type == Event::KeyPressed)
+					   if (e.key.code == Keyboard::Enter) play.game();
+					   else if (e.key.code == Keyboard::Escape) return 0;
+			   }
+			   window.clear(Color::Green);
+			   window.draw(s);
+
+
+			   Font font;//шрифт 
+			   font.loadFromFile("CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
+			   Text menutext("", font, 25);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+			   menutext.setFillColor(Color::Cyan);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+			   menutext.setString("Нажмите Enter чтобы начать игру \n \n или Esc чтобы выйти");
+			   menutext.setPosition(55, 200);
+			   window.draw(menutext);//рисую этот текст
+
+			   Text recordtext("", font, 20);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+			   recordtext.setFillColor(Color::Cyan);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+			   std::ostringstream record;    // объявили переменную
+			   record << maxscore;		//занесли в нее число очков, то есть формируем строку
+			   recordtext.setString("Игровой рекорд: " + record.str() + " очков.");//задаем строку тексту и вызываем сформированную выше строку методом .str() 
+			   recordtext.setPosition(210, 630);
+			   window.draw(recordtext);//рисую этот текст
+
+			   window.display();
+		   }
+	   }
+};
+
+
+	
