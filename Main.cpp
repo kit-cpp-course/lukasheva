9efb8567162b07d07c5045fb8d530b96fdb8b6af
lukashevaev <46032891+lukashevaev@users.noreply.github.com>
2018-12-27 17:23:58 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <SFML\Graphics.hpp>
+#include <time.h>
+#include "windows.h"
+#include <sstream>
+#include "Game.h"
+#include "Record.h"
+#include "Menu.h"
+
+using namespace sf;
+
+int main()
+{
+		Menu menu;
+		menu.menu();
+}
